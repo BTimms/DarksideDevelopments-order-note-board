0882f0ba97da8946ef0694e309e169a828fd53f9
Brian <brian.timakela@hotmail.com>
2026-05-25 01:18:41 +0100

    Edited the Note eloquent model file for the database

--- app/Models/Note.php
+++ app/Models/Note.php
@@ -4,7 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * “This is an Eloquent model connected to a Notes database table.”
+*/
 class Note extends Model
 {
-    //
+    protected $fillable = [
+        'order_number',
+        'message',
+        'author',
+    ];
 }
