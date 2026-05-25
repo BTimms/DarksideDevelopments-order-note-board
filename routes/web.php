415e99bc9b0fe5cece3954a96f535e92fed4f9f9
Brian <brian.timakela@hotmail.com>
2026-05-25 02:44:24 +0100

    Added the route to the web.php file that will display the order note board page

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Route::get('/notes', [\App\Http\Controllers\Api\NotesController::class, 'index']);
 Route::post('/notes', [\App\Http\Controllers\Api\NotesController::class, 'store']);
+
+Route::get('/', function () {
+    return view('layouts.order');
+});
