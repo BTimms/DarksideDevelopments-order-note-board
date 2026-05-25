c2ae5e49420af7919f71216b44bd4fed21fd839b
Brian <brian.timakela@hotmail.com>
2026-05-25 02:20:17 +0100

    Added routes to the web.php file that will be used to retrieve all notes from the controllers

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/notes', [\App\Http\Controllers\Api\NotesController::class, 'index']);
+Route::post('/notes', [\App\Http\Controllers\Api\NotesController::class, 'store']);
